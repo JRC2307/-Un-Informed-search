78a1ea7cc4f467fecfb051ab8cbc488a510e24b6
estebanquintana6 <a01184855@itesm.mx>
2018-09-16 20:27:44 -0500

    Goal and start state fixed, and areEqual function implemented

--- lab3_parser.py
+++ lab3_parser.py
@@ -2,7 +2,7 @@
 # (A, C); X; X
 
 stacks = []
-
+output_stack = []
 
 class Stack:
     name = ""
@@ -18,36 +18,42 @@ class Stack:
     def to_string(self):
         aux = ""
         for c in self.containers:
-            aux = aux + c + " ;"
+            aux = aux + c + ";"
 
         print('Stack: ' + self.name + " Containers: " + aux)
 
 
-def initialize_containers(raw_string):
-    parsed_containers = raw_string.split(';')
+def initialize_containers(raw_string, destiny):
+    parsed_stacks = raw_string.split(';')
+    for x in range(0, len(parsed_stacks)):
+        parsed_containers = parsed_stacks[x].strip().strip('(').strip(')')
+        parsed_elements = parsed_containers.split(',')
+        stack_constructor = Stack(str(x),[])
 
-    for x in range(0, len(parsed_containers)):
-        parsed_containers[x] = parsed_containers[x].strip('()')
-        elements = parsed_containers[x].split(',')
-        for y in range(0, len(elements)):
-            stacks[x].add_container(elements[y])
+        for e in range(0, len(parsed_elements)):
+            stack_constructor.add_container(parsed_elements[e].strip(" "))
 
+        destiny.append(stack_constructor)
 
-no_stacks = int(input('Enter the number of stacks: '))
-print(no_stacks)
+def are_equal():
+    for x in range(0,len(stacks)):
+        for y in range(0,len(stacks[x].containers)):
+            if stacks[x].containers[y] != output_stack[x].containers[y]: return False
+        return True
 
-unparsed_stacks = input('Enter the name of the stacks: ')
+no_stacks = int(input('Enter the maximum number of containers: '))
+unparsed_stacks = input('Enter the input stacks: ')
+uparsed_output = input('Enter the output stacks: ')
 
-parsed_stacks = unparsed_stacks.split(';')
+initialize_containers(unparsed_stacks, stacks)
+initialize_containers(uparsed_output, output_stack)
 
-for x in range(0, len(parsed_stacks)):
-    stack_constructor = Stack(parsed_stacks[x].strip().strip('()'), [])
-    stacks.append(stack_constructor)
+print("\nInitial state: ")
+for e in stacks:
+    e.to_string()
 
-unparsed_containers = input('Enter the container s string: ')
+print("Goal state: ")
+for g in output_stack:
+    g.to_string()
 
-initialize_containers(unparsed_containers)
-
-
-for x in range(0, len(stacks)):
-    stacks[x].to_string()
+print(are_equal())
